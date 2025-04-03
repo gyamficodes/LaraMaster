a9bc2d930a8f7d20c061c27f969e41fc06e5cc7a
gyamficodes <gyamfijohn08@gmail.com>
2025-04-03 09:19:22 +0000

    DB relation btn user and post is done.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\User;
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+    protected $table = "posts";
+  protected  $filliable = ['title', 'body', 'user_id'];
+
+ public function  user(){
+    return $this->belongsTo(User::class);
+ }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use App\Models\Post;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -22,6 +22,13 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    protected $table = "users";
+
+ public function posts(){
+    return $this->hasMany(Post::class);
+ }
+
 
     /**
      * The attributes that should be hidden for serialization.
