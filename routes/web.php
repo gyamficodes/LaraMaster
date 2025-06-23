c9a141334933bff7c28ebe0d32fb23bbf68ad07f
gyamficodes <gyamfijohn08@gmail.com>
2025-06-23 23:06:26 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,10 @@ use function Pest\Laravel\get;
 Route::get('/' , WelcomeController::class)->name('/');
 
 // Profile routes
-Route::sengleton('/profile', ProfileController::class)
-    ->name('profile')
+Route::singleton('/profile', ProfileController::class)
+    ->name('profile', 'index')
     ->middleware('auth'); // Ensure the profile route is protected by authentication
+
 
 Route::get('/posts', [PostController::class, 'index'])->name('post.index');   // to show posts
 Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');  // to show create form
